14d3b7f84d14c88b7486dff821e9e60776d52ad5
Danil <105702087+Danisimo0@users.noreply.github.com>
2023-04-06 13:35:55 +0600

    up

--- HW/23.01/chat/routing.py
+++ HW/23.01/chat/routing.py
@@ -1,29 +1,7 @@
-from django.urls import path
-from django.urls.conf import include
 from django.urls import re_path
 
-from . import views
-from django.contrib import admin
+from . import consumers
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from chat import routing
-
-application = ProtocolTypeRouter({
-    # http protocol router
-    'http': URLRouter([
-        path('admin/', admin.site.urls),
-        path('accounts/', include('django.contrib.auth.urls')),
-        path('signup/', views.SignUp.as_view(), name='signup'),
-        path('/home/', views.HomePageView.as_view(), name='home'),
-        re_path(r'^sw.js$', views.ServiceWorkerView.as_view(), name='sw.js'),
-    ]),
-
-    # websocket protocol router
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            routing.websocket_urlpatterns
-        )
-    ),
-
-})
+websocket_urlpatterns = [
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+]
--- HW/23.01/chat/views.py
+++ HW/23.01/chat/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import View
 from django.shortcuts import render
 
 def index(request):
@@ -8,3 +13,30 @@ def chat_room(request, room_name):
     return render(request, 'chat/chat_room.html', {
         'room_name': room_name
     })
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class ServiceWorkerView(TemplateView):
+    content_type = 'application/javascript'
+    template_name = 'sw.js'
+
+class SignUp(View):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+    def get_success_url(self):
+        return self.success_url
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(self.get_success_url())
+        return render(request, self.template_name, {'form': form})
