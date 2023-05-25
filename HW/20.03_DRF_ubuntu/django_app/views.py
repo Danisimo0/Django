ceb1ba60b55f5e2bca6155226b4f9ecce43d45ab
Danisimo0 <dvassilec655@gmail.com>
2023-05-25 20:08:40 +0600

    modified:   .idea/misc.xml 	modified:   django_app/views.py 	modified:   django_settings/__pycache__/settings.cpython-310.pyc 	modified:   django_settings/__pycache__/urls.cpython-310.pyc 	modified:   django_settings/settings.py 	modified:   django_settings/urls.py 	django_app/__pycache__/ 	django_settings/__pycache__/wsgi.cpython-310.pyc

--- HW/20.03_DRF_ubuntu/django_app/views.py
+++ HW/20.03_DRF_ubuntu/django_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, World!"})
 
 # Create your views here.
--- HW/20.03_DRF_ubuntu/django_settings/urls.py
+++ HW/20.03_DRF_ubuntu/django_settings/urls.py
@@ -16,7 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django_app.views import HelloView
+
+router = routers.DefaultRouter()
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('hello/', HelloView.as_view(), name='hello'),
 ]
+
+
